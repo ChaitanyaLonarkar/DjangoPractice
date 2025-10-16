49841160b4a95cee1fa7b0131220e0e33f579baa
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2025-10-16 23:54:17 +0530

    data is going to database

--- myapp/crudWithAuth/urls.py
+++ myapp/crudWithAuth/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
+from . import user_view
 
 urlpatterns = [
     path('register/', views.Register, name='register'),
     path('login/', views.Login, name='login'),
     path('home/', views.Home, name='home'),
     path('logout/', views.Logout, name='logout'),
+    path('create/', user_view.create_portfolio, name='create_portfolio'),
+    path('', user_view.preview, name='portfolio_preview'),
     # path('list/', views.list_users, name='list_users'),
     # path('edit/<uuid:id>/', views.edit_user, name='edit_user'),
     # path('delete/<uuid:id>/', views.delete_user, name='delete_user'),
